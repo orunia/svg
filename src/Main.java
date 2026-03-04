846c781ecc7ebc5e53d1b052568a607b4f747ec7
orunia <ikordyga@gmail.com>
2026-03-04 18:06:03 +0100

    labo1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        Point point = new Point();
+
+        // Zadanie 1
+        Point point = new Point(3, 4);
+        System.out.println(point);
         System.out.println(point.x + " " + point.y);
-        Point point1 = new Point();
-        System.out.println(point1.toString());
-        System.out.println(point1.toSvg());
+
+        // Zadanie 2
+        System.out.println(point.toSvg());
+
+        // Zadanie 3
+        point.translate(2, 3);
+        System.out.println("Po translate: " + point);
+
+        Point newPoint = point.translated(-1, -1);
+        System.out.println("Nowy punkt (translated): " + newPoint);
+        System.out.println("Oryginalny punkt: " + point);
+
+        // Zadanie 4
+        Segment s1 = new Segment(new Point(0, 0), new Point(3, 4));
+        Segment s2 = new Segment(new Point(0, 0), new Point(6, 8));
+        Segment s3 = new Segment(new Point(1, 1), new Point(2, 2));
+
+        System.out.println("Długość s1: " + s1.length());
+        System.out.println("Długość s2: " + s2.length());
+        System.out.println("Długość s3: " + s3.length());
+
+        // Zadanie 5
+        Segment[] segments = {s1, s2, s3};
+        Segment longest = Segment.longestSegment(segments);
+
+        if (longest != null) {
+            System.out.println("Najdłuższy segment ma długość: " + longest.length());
+        }
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,13 +1,38 @@
 public class Point {
-    public double x=6, y=5;
+    public double x;
+    public double y;
 
-    public String toString(){
-        String napis = "x: " + this.x + ", y: " + y;
-        return napis;
+    // Konstruktor bezargumentowy
+    public Point() {
+        this.x = 0;
+        this.y = 0;
     }
 
-    public String toSvg(){
-        String napis = "<circle r=\"45\" cx=\""+x+"\" cy=\""+y+"\" fill=\"red\" />";
-        return napis;
+    // Konstruktor z parametrami
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Zadanie 1
+    @Override
+    public String toString() {
+        return "x: " + x + ", y: " + y;
+    }
+
+    // Zadanie 2
+    public String toSvg() {
+        return "<circle r=\"5\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"red\" />";
+    }
+
+    // Zadanie 3 - zmienia aktualny punkt
+    public void translate(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    // Zadanie 3 - tworzy nowy przesunięty punkt
+    public Point translated(double dx, double dy) {
+        return new Point(this.x + dx, this.y + dy);
     }
 }
